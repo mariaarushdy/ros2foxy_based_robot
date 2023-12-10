6bc8ae098d22722516da4b79a5800e879cca14eb
mariaarushdy <mariaroshdy17@gmail.com>
2023-12-10 22:55:08 +0200

    project 1

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -10,6 +10,8 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
 
 def generate_launch_description():
 
@@ -22,13 +24,16 @@ def generate_launch_description():
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'false'}.items()
     )
-
     # Include the Gazebo launch file, provided by the gazebo_ros package
+   
+    gazebo_params_file =os.path.join(
+                    get_package_share_directory(package_name),'config','gazebo_param.yaml'
+                )
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),launch_arguments={'extra_gazebo_args': '--ros-args --params-file '+ gazebo_params_file}.items()
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
@@ -38,10 +43,30 @@ def generate_launch_description():
                         output='screen')
 
 
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["joint_broad"],
+    )
+
+    delayed_diff_drive_and_joint_spawner = RegisterEventHandler(
+    event_handler=OnProcessExit(
+        target_action=spawn_entity,
+        on_exit=[diff_drive_spawner, joint_broad_spawner],
+    )
+)
 
     # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+        delayed_diff_drive_and_joint_spawner,
+        #joint_broad_spawner
     ])
